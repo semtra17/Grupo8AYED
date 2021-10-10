57ae5dd31da90ffa404fc4a2ae098add3d72849e
Luciano Aizar Assefh <63946743+semtra17@users.noreply.github.com>
2021-10-10 19:39:41 -0300

    Pushing something

--- main.cpp
+++ main.cpp
@@ -17,13 +17,15 @@ using namespace std;
 int main()
 {
 
-    ListaUsuario* LISTA_USUARIOS = nuevaListaUsuario();
-    cargarListaUsuarioDesdeArchivo(LISTA_USUARIOS);
-    printListaUsuario(LISTA_USUARIOS);
+//    ListaUsuario* LISTA_USUARIOS = nuevaListaUsuario();
+//    cargarListaUsuarioDesdeArchivo(LISTA_USUARIOS);
+//    printListaUsuario(LISTA_USUARIOS);
 
-    ListaVinos * LISTA_VINOS = nuevaListaVinos();
-    cargarListaVinosDesdeArchivo(LISTA_VINOS);
-    printListaVinos(LISTA_VINOS);
+//    ListaVinos * LISTA_VINOS = nuevaListaVinos();
+//    cargarListaVinosDesdeArchivo(LISTA_VINOS);
+//    printListaVinos(LISTA_VINOS);
+
+
     ListaSelecciones *LISTA_SELECCION_MENSUAL = nuevaListaSelecciones();
     cargarListaSeleccionesDesdeArchivo(LISTA_SELECCION_MENSUAL);
     printListaSelecciones(LISTA_SELECCION_MENSUAL);
